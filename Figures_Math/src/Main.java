6e5152b3252c492ef0d2400062307731bcbd05c0
DorV8 <play_game_in_pc@mail.ru>
2023-01-12 15:43:52 +0300

    1

--- Figures_Math/src/Main.java
+++ Figures_Math/src/Main.java
@@ -27,7 +27,7 @@ public class Main {
                     }
                 }
                 Figures.triangle result = new Figures.triangle(input[0], input[1], input[2]);
-                System.out.println("Что необходимо расчитать?: ");
+                System.out.println("Что необходимо расчитать? : ");
                 System.out.println("""
                         1. Площадь треугольника
                         2. Периметр треугольника
@@ -45,7 +45,7 @@ public class Main {
                 System.out.println("Введите ширину: ");
                 input[1] = Math.abs(scanner.nextDouble());
                 Figures.rectangle result = new Figures.rectangle(input[0], input[1]);
-                System.out.println("Что необходимо расчитать?: ");
+                System.out.println("Что необходимо расчитать? : ");
                 System.out.println("""
                         1. Периметр прямоугольника
                         2. Площадь прямоугольника
@@ -79,7 +79,7 @@ public class Main {
                 System.out.println("Введите радиус: ");
                 double input = scanner.nextDouble();
                 Figures.circle result = new Figures.circle(input);
-                System.out.println("Что необходимо расчитать?: ");
+                System.out.println("Что необходимо расчитать? : ");
                 System.out.println("""
                         1. Площадь круга
                         2. Периметр круга
